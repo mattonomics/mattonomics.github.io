d8fc6f50981d62da1e43b676efa485880295a6d7
Matt Gross <mattonomics@gmail.com>
2020-10-12 10:29:33 -0400

    First commit of this React app. No routing in place, but the next step is to offload all the content to WordPress and either integrate with graphql or REST API.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// custom componenets
+import Navigation from './components/Navigation';
+import HeaderHero from './components/HeaderHero';
+
+// bring in the css
+import './App.sass';
+
+// main application control
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Navigation />
+        <HeaderHero />
+      </>
+    );
+  }
 }
 
 export default App;
