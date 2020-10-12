4773b7de5762bfa95d5e19b91938700d4d036fad
Matt Gross <mattonomics@gmail.com>
2020-10-12 10:38:17 -0400

    Updates

---
+++ precache-manifest.6fd7417dfa777e6ed48fe047b902f5bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949a87c6cecd9b4b7506fd23dfaf5037",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b1e628238a655e380b8",
+    "url": "/static/css/main.2ad2c543.chunk.css"
+  },
+  {
+    "revision": "f451c085d8a4d4d8adfb",
+    "url": "/static/js/2.6abd43f3.chunk.js"
+  },
+  {
+    "revision": "ea826bf8f1d3957960299a7ec9f9a7ae",
+    "url": "/static/js/2.6abd43f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1e628238a655e380b8",
+    "url": "/static/js/main.1b761ec7.chunk.js"
+  },
+  {
+    "revision": "77cc38363ceab31807fc",
+    "url": "/static/js/runtime-main.32222e00.js"
+  }
+]);
